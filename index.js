8811a7ba107a0531eafdbf8c28399b80477ec6e4
wuzhanwei <zhanwei.wu@dbjtech.com>
2018-06-27 16:50:10 +0800

    feat: add host map for proxy

--- index.js
+++ index.js
@@ -2,29 +2,46 @@ const request = require('request')
 const http = require("http")
 const url = require("url")
 const net = require('net')
+const httpProxy = require('http-proxy')
 
-const server = http.createServer((req, res) => {
-	const { protocol, host } = url.parse(req.url)
-	if (!protocol || !host) {
-		res.end('Hello proxy user!')
-		return
-	}
-	console.log(`Proxy HTTP request for: ${protocol}//${host}`)
-	req.pipe(request(req.url)).pipe(res)
-})
+let server
 
-server.addListener('connect', (req, socket, bodyhead) => {
-	const [hostDomain, hostPort] = req.url.split(':')
-	console.log(`Proxy HTTPS request for: ${hostDomain}:${hostPort}`)
+if (process.env.TARGET) {
+	const hostMap = JSON.parse(process.env.TARGET)
+	const proxy = httpProxy.createProxyServer({ changeOrigin: true })
+	server = http.createServer((req, res) => {
+		const src = req.headers.host + req.url
+		const target = hostMap[src] || hostMap[req.headers.host]
+		if (!target) {
+			res.end('Hello proxy user!')
+			return
+		}
+		console.log(`Proxy HTTP request for: ${src} -> ${target}${req.url}`)
+		proxy.web(req, res, { target })
+	})
+} else {
+	server = http.createServer((req, res) => {
+		if (!protocol || !host) {
+			res.end('Hello proxy user!')
+			return
+		}
+		console.log(`Proxy HTTP request for: ${protocol}//${host}`)
+		req.pipe(request(req.url)).pipe(res)
+	})
 
-	const proxySocket = new net.Socket()
-	proxySocket.connect(hostPort, hostDomain, function () {
-		proxySocket.write(bodyhead)
-		socket.write(`HTTP/${req.httpVersion} 200 Connection established\r\n\r\n`)
+	server.addListener('connect', (req, socket, bodyhead) => {
+		const [hostDomain, hostPort] = req.url.split(':')
+		console.log(`Proxy HTTPS request for: ${hostDomain}:${hostPort}`)
 
-		proxySocket.pipe(socket)
-		socket.pipe(proxySocket)
-	}).on('error', console.error)
-})
+		const proxySocket = new net.Socket()
+		proxySocket.connect(hostPort, hostDomain, function () {
+			proxySocket.write(bodyhead)
+			socket.write(`HTTP/${req.httpVersion} 200 Connection established\r\n\r\n`)
+
+			proxySocket.pipe(socket)
+			socket.pipe(proxySocket)
+		}).on('error', console.error)
+	})
+}
 
 server.listen(process.env.PORT || 80)
--- readme.md
+++ readme.md
@@ -19,6 +19,26 @@ docker-compose -f docker-compose.yml up
 
 ### http/https
 
+If TARGET env var is porvided, proxy traffic according to the host map.
+If TARGET env var is empty, act as a common proxy server.
+
+```bash
+$env:TARGET='{"baidu.localhost:8080":"https://www.baidu.com", "google.localhost:8080":"https://www.google.com"}'
+node .
+```
+
+TARGET config example.
+
+```json
+{
+	"localhost:8080/helloworld":"https://wt-18e127c6f4b8a13508b25fa5c646c8a2-0.sandbox.auth0-extend.com/hello/world",
+	"localhost:8080/":"https://wt-18e127c6f4b8a13508b25fa5c646c8a2-0.sandbox.auth0-extend.com/index.html",
+	"localhost:8080":"https://wt-18e127c6f4b8a13508b25fa5c646c8a2-0.sandbox.auth0-extend.com",
+	"baidu.localhost:8080":"https://www.baidu.com",
+	"google.localhost:8080":"https://www.google.com"
+}
+```
+
 ```bash
 docker run -it -p 8123:80 vincent/proxy:nodejs
 ```
